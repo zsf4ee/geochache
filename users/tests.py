8964649eb9fb0aa9d5e737aa00dc028795de3d31
EmmaMeiAndreas <142939682+EmmaMeiAndreas@users.noreply.github.com>
2023-10-18 22:59:01 -0400

    Create tests.py with sample tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from . models import User, Geocache, Find, Comment
+from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
+from django.test import client
 
 # Create your tests here.
+
+# YourTestClass Code from https://developer.mozilla.org/en-US/docs/Learn/Server-side/Django/Testing
+
+class YourTestClass(TestCase):
+    def setUp(self):
+        # Setup run before every test method.
+        pass
+
+    def tearDown(self):
+        # Clean up run after every test method.
+        pass
+
+    def test_something_that_will_pass(self):
+        self.assertFalse(False)
+
+    def test_something_that_will_fail(self):
+        self.assertTrue(False)
